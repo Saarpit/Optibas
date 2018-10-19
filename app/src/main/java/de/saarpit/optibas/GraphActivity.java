c87a2448bed6a192762c3174f6179c8c3cd4cf4d
Eric Peters <peters.eric@gmx.de>
2018-10-19 22:14:51 +0200

    - Einbau der Default Verteilungen der Basalrate - Darstellung im Graph - Auslesen eines Wertes in einer anderen Activity

--- app/src/main/java/de/saarpit/optibas/GraphActivity.java
+++ app/src/main/java/de/saarpit/optibas/GraphActivity.java
@@ -1,12 +1,17 @@
 package de.saarpit.optibas;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.ColorRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import de.saarpit.optibas.core.Constants;
 import de.saarpit.optibas.ui.graph.GraphFragment;
 
 public class GraphActivity extends AppCompatActivity {
@@ -15,18 +20,55 @@ public class GraphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.graph_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, GraphFragment.newInstance())
-                    .commitNow();
-        }
+
+        SharedPreferences sharedPref = getApplication().getSharedPreferences("main", Context.MODE_PRIVATE);
+        String name = sharedPref.getString(getString(R.string.activity_main_textFieldName),
+                new String());
+
+        TextView debugger = (TextView)findViewById(R.id.debugTextView);
+        debugger.setText(name);
 
         GraphView graph = (GraphView) findViewById(R.id.graphView);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
-        });
-        graph.addSeries(series);
+
+        // set manual X bounds
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(23);
+
+        // enable scaling and scrolling
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+
+        LineGraphSeries<DataPoint> seriesKleinkinder =
+                new LineGraphSeries<>(generateData(Constants.BASALVERTEILUNG_KLEINKINDER));
+        seriesKleinkinder.setDrawDataPoints(true);
+        seriesKleinkinder.setColor(getResources().getColor(R.color.graphColorKleinkinder));
+        seriesKleinkinder.setTitle("Kleinkinder");
+        graph.addSeries(seriesKleinkinder);
+
+        LineGraphSeries<DataPoint> seriesKinder =
+                new LineGraphSeries<>(generateData(Constants.BASALVERTEILUNG_KINDER));
+        seriesKinder.setDrawDataPoints(true);
+        seriesKinder.setColor(getResources().getColor(R.color.graphColorKinder));
+        seriesKinder.setTitle("Kinder");
+        graph.addSeries(seriesKinder);
+
+        LineGraphSeries<DataPoint> seriesErwachsene =
+                new LineGraphSeries<>(generateData(Constants.BASALVERTEILUNG_ERWACHSENE));
+        seriesErwachsene.setDrawDataPoints(true);
+        seriesErwachsene.setColor(getResources().getColor(R.color.graphColorErwachsene));
+        seriesErwachsene.setTitle("Erwachsene");
+        graph.addSeries(seriesErwachsene);
+    }
+
+    private DataPoint[] generateData(double[] values) {
+        DataPoint[] dataValues = new DataPoint[24];
+        for (int i=0; i<24; i++) {
+            double x = i;
+            double y = values[i];
+            DataPoint v = new DataPoint(x, y);
+            dataValues[i] = v;
+        }
+        return dataValues;
     }
 }
--- app/src/main/java/de/saarpit/optibas/MainActivity.java
+++ app/src/main/java/de/saarpit/optibas/MainActivity.java
@@ -1,9 +1,13 @@
 package de.saarpit.optibas;
 
+import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startResultActivity(View v){
-        startActivity(new Intent(MainActivity.this, Main2Activity.class));
+        SharedPreferences sharedPref = getApplication().getSharedPreferences("main", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        EditText textFieldName = (EditText)findViewById(R.id.textFieldName);
+        editor.putString(getString(R.string.activity_main_textFieldName),
+                String.valueOf(textFieldName.getText()));
+        editor.commit();
+
+        startActivity(new Intent(MainActivity.this, GraphActivity.class));
     }
 }
